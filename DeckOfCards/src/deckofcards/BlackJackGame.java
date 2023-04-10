62e94601e5bc4087020fb576651392aeb54755a5
Zach Royer <zach.royer14@gmail.com>
2023-04-10 14:47:01 -0600

    Update BlackJackGame.java

--- DeckOfCards/src/deckofcards/BlackJackGame.java
+++ DeckOfCards/src/deckofcards/BlackJackGame.java
@@ -37,6 +37,7 @@ public class BlackJackGame extends Deck {
 			System.out.println(".");
 			System.out.println(".");
 			System.out.println(".");
+			System.out.println(".");
 
 			displayDealerHand();
 			displayPlayerHand();
