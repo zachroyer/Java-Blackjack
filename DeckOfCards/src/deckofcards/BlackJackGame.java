84b5573b31ae992338352da06d3c52088da8aa9b
Zach Royer <zach.royer14@gmail.com>
2023-04-12 21:55:19 -0600

    Outsourced methods away from BlackJackGame Methods now in Hand class

--- DeckOfCards/src/deckofcards/BlackJackGame.java
+++ DeckOfCards/src/deckofcards/BlackJackGame.java
@@ -10,12 +10,15 @@ import java.util.Scanner;
  *
  * @author zachroyer
  */
-public class BlackJackGame extends Deck {
+public class BlackJackGame extends Hand {
 
 	// attributes
-	private int nextCard = 0;
 
-	private int playerBalance = 50;
+	public BlackJackGame(String deckType, int balance) {
+		super(deckType);
+		balance = 50;
+
+	}
 
 	public static void main(String[] args) {
 
@@ -24,60 +27,5 @@ public class BlackJackGame extends Deck {
 		System.out.println(mainGame.getDeck().toString());
 
 	}
-
-	Scanner scnr = new Scanner(System.in);
-
-	// methods
-	public BlackJackGame(String deckType) {
-		super(deckType);
-
-		this.shuffle();
-
-		System.out.println("Press p to play...");
-		String temp = scnr.nextLine();
-
-		if ("p".equals(temp)) {
-			System.out.println("Place Your Bet:");
-			String bet = scnr.nextLine();
-			while (Integer.parseInt(bet) > playerBalance) {
-				System.out.println("Bet cannot be higher than your balance\n Place Your Bet:");
-				bet = scnr.nextLine();
-			}
-
-			System.out.println("\nDealing cards \n.\n.\n.");
-
-
-
-		}
-	}
-
-	private StringBuilder displayDealerHand() {
-		StringBuilder dealerHand = new StringBuilder();
-		for (int i = 0; i <= nextCard; i++) {
-			if ((getCard(i).isDealer())) {
-				dealerHand = dealerHand.append(getCard(i).toString());
-
-			}
-		}
-		return dealerHand;
-	}
-
-	private StringBuilder displayPlayerHand() {
-		StringBuilder playerHand = new StringBuilder();
-		for (int i = 0; i < nextCard; i++) {
-			if ((!getCard(i).isDealer())) {
-				playerHand = playerHand.append(getCard(i).toString());
-
-			}
-		}
-		return playerHand;
-	}
-
-	private String dealNextCard(String owner) {
-		this.getCard(nextCard).assignTo(owner);
-		this.getCard(nextCard).flipCard();
-		int temp = nextCard;
-		nextCard += 1;
-		return this.getCard(temp).toString();
-	}
 }
+
--- DeckOfCards/src/deckofcards/Hand.java
+++ DeckOfCards/src/deckofcards/Hand.java
@@ -4,22 +4,79 @@
  */
 package deckofcards;
 
+import java.util.Scanner;
 /**
  *
  * @author zachroyer
  */
 import java.util.ArrayList;
 
-public class Hand  {
-	//ATTRIBUTES
+public class Hand extends Deck {
+	// ATTRIBUTES
 
-	
+	int nextCard;
+	private int playerBalance;  //make a setter for the balance 
 
-	//METHODS
+	Scanner scnr = new Scanner(System.in);
 
-	public Hand() {	
-		
-	}
+	// METHODS
+
+	public Hand (String deckType) {	
+		super(deckType);
+		nextCard = 0;	
+
+
+		this.shuffle();
+
+		System.out.println("Press p to play...");
+		String temp = scnr.nextLine();
+
+		if ("p".equals(temp)) {
+			System.out.println("Place Your Bet:");
+			String bet = scnr.nextLine();
+			while (Integer.parseInt(bet) > playerBalance) {
+				System.out.println("Bet cannot be higher than your balance\n Place Your Bet:");
+				bet = scnr.nextLine();
+			}
+
+			System.out.println("\nDealing cards \n.\n.\n.");
+
+
+
+
+	}}
 
 	//Push Method
+
+	private StringBuilder displayDealerHand() {
+		StringBuilder dealerHand = new StringBuilder();
+		for (int i = 0; i <= nextCard; i++) {
+			if ((getCard(i).isDealer())) {
+				dealerHand = dealerHand.append(getCard(i).toString());
+
+			}
+		}
+		return dealerHand;
+	}
+
+	private StringBuilder displayPlayerHand() {
+		StringBuilder playerHand = new StringBuilder();
+		for (int i = 0; i < nextCard; i++) {
+			if ((!getCard(i).isDealer())) {
+				playerHand = playerHand.append(getCard(i).toString());
+
+			}
+		}
+		return playerHand;
+	}
+
+	private String dealNextCard(String owner) {
+		this.getCard(nextCard).assignTo(owner);
+		this.getCard(nextCard).flipCard();
+		int temp = nextCard;
+		nextCard += 1;
+		return this.getCard(temp).toString();
+	}
+}
+
 }
